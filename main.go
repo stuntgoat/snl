e35b85e9e95c4276afc78f4dda85c8ef55ab5c3f
Isaac Gonzalez <isaac@overttone.com>
2013-08-04 22:16:09 -0700

    simplify invocation; add ability to capture percentage of the total lines

--- main.go
+++ main.go
@@ -1,7 +1,3 @@
-// TODO: - help string for positional arguments
-// - streams
-// - split percentage of the data into train and test files
-
 package main
 
 import (
@@ -15,83 +11,115 @@ import (
 	"time"
 )
 
+
+var SAMPLE_TYPE int
+const (
+	INTEGER = iota
+	PERCENTAGE
+	)
+
+var SAMPLE_MAP map[int]string
+var SAMPLE_VALUE int // either a percentage or an integer value
+
+var command = os.Args[0]
+var invocation = fmt.Sprintf("%s [[sample size]%%] [file path]\n", command)
+
 var logger *log.Logger
-var SAMPLE map[int]string
-var count int
+
+// flag.Usage help message override
+var Usage = func() {
+	fmt.Fprintf(os.Stderr, "Usage: %s", invocation)
+}
 
 func init() {
 	logger = log.New(os.Stderr, "[SNL] ", log.LstdFlags|log.Lshortfile)
-	SAMPLE = make(map[int]string)
+	SAMPLE_MAP = make(map[int]string)
+	rand.Seed(time.Now().UTC().UnixNano())
 
+	flag.Usage = Usage
+	flag.Parse()
+}
+
+// parseValue determines if the value is a percentage or
+// an integer, and sets the global value `SAMPLE_VALUE`,
+func parseValue(s string) {
+	var value string
+	if string(s[len(s) - 1]) == "%" {
+		SAMPLE_TYPE = PERCENTAGE
+		value = s[:len(s)-1]
+	} else {
+		SAMPLE_TYPE = INTEGER
+		value = s
+	}
+
+	// convert value to integer
+	intValue, err := strconv.Atoi(value)
+	if err != nil {
+		logger.Printf("[Error] error converting sample_size: %s to integer: %s", value, err)
+		fmt.Printf("Usage: %s", invocation)
+		os.Exit(1)
+	}
+	SAMPLE_VALUE = intValue
+}
+
+// parseFile validates a string and returns an *os.File
+func parseFile(s string) (file *os.File){
+	if s == "" {
+		logger.Print("[Error] missing filename argument")
+		fmt.Printf("Usage: %s", invocation)
+		os.Exit(1)
+	}
+
+	file, err := os.Open(s)
+	if err != nil {
+		logger.Fatalf("[Error] error opening %s: %s", s, err)
+	}
+
+	return file
 }
 
 func main () {
-	rand.Seed(time.Now().UTC().UnixNano())
-	sample_size_help := "The number of lines to sample from a file"
-	var sampleSize = flag.Int("sample_size", 0, sample_size_help)
+	var count int // a count of how many lines have been collected
+	var candidate int // tmp variable for choosing a random number
+	var done int // number of lines printed so far
+	var totalOut int // number of total lines to print after calling parseValue
 
-	filename_help := "The file which to sample from"
-	var filename = flag.String("file", "", filename_help)
+	sampleSize := flag.Arg(0)
+	parseValue(sampleSize)
 
-	flag.Parse()
+	fileName := flag.Arg(1)
 
-	// try to get the positional argument for count argument
-	if *sampleSize == 0 {
-		stringSize := flag.Arg(0)
-		s, err := strconv.Atoi(stringSize)
-		sampleSize = &s
-		if err != nil {
-			logger.Fatalf("[Error] error converting %s to integer: %s", stringSize, err)
-		}
-	}
-
-	// try to get the positional argument for filename argument
-	if *filename == "" {
-		f := flag.Arg(1)
-		if f == "" {
-			logger.Fatal("[Error] missing filename")
-		}
-		filename = &f
-	}
-
-	file, err := os.Open(*filename)
-
-	if err != nil {
-		logger.Fatalf("[Error] error opening %s: %s", *filename, err)
-	}
+	file := parseFile(fileName)
 	defer file.Close()
-
-	// TODO: - for streams,
-	// we need to store the first `count` items from the file in a hash or circular buffer
-	// then we replace lines at random in something that ends up being a random
-	// distribution
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		// for files
-		// randomly store this line after first collecting the first `sampleSize` items
-		SAMPLE[count] = fmt.Sprint(scanner.Text())
+		// store all lines in a map with a line number index
+		SAMPLE_MAP[count] = fmt.Sprint(scanner.Text())
 		count++
 	}
 
 	// a log of which line numbers we have seen
 	seen := make(map[int]bool)
 
-	var candidate int
-	var done int
+	// calulate the number of values we need to print to stdout
+	if SAMPLE_TYPE == INTEGER {
+		totalOut = SAMPLE_VALUE
+	} else if SAMPLE_TYPE == PERCENTAGE {
+		totalOut = int((float64(SAMPLE_VALUE) / 100.0) * float64(count))
+	}
+
 	for {
 		candidate = rand.Intn(count)
 
-		// if we haven't seen this before, print to stdout
+		// if we haven't printed this line before, print to stdout
 		if seen[candidate] != true {
-			fmt.Println(SAMPLE[candidate])
+			fmt.Println(SAMPLE_MAP[candidate])
 			seen[candidate] = true
 			done++
 		}
-		if done == *sampleSize {
-			goto DONE
+		if done == totalOut {
+			os.Exit(0)
 		}
 	}
-DONE:
-	os.Exit(0)
 }
